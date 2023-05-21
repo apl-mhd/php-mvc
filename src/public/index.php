8fbcc0fd22b2febfa3754ed8bd8da3b0354b8f5d
apl-mhd <mmahmud161036@bscse.uiu.ac.bd>
2023-05-21 23:05:52 +0600

    autoload

--- src/public/index.php
+++ src/public/index.php
@@ -1,25 +1,14 @@
 <?php
 
 
-//require_once('PaymentGateway/Stripe/Transaction.php');
-//require_once('PaymentGateway/Paddle/CustomerProfile.php');
-//require_once('../PaymentGateway/Paddle/Transaction.php');
-//require_once('./namespace/app/PaymentGateway/Paddle/Transaction.php');
-
-echo __DIR__ . PHP_EOL;
-spl_autoload_register(function($class){
-
-    $path  = __DIR__ . '/../' . lcfirst(str_replace('\\', '/', $class). '.php');
-    require $path;
-});
-
+require __DIR__ . '/../vendor/autoload.php';
 
 
 use App\PaymentGateway\Paddle\Transaction;
-use App\PaymentGateway\Stripe\Transaction as t;
-use App\PaymentGateway\Paddle\CustomerProfile;
 
- $paddleTransaction = new t();
 
- var_dump($paddleTransaction);
+$paddleTransaction = new Transaction();
+
+ //var_dump($paddleTransaction);
+
 
