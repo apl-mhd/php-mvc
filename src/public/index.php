ead442347632f4a2e8ee3dcaedc1cc442d5814f9
apl-mhd <mmahmud161036@bscse.uiu.ac.bd>
2023-07-12 09:27:45 +0600

    week map

--- src/public/index.php
+++ src/public/index.php
@@ -1,35 +1,17 @@
 <?php
 
-use App\Router;
-use App\Config;
-use App\Container;
+declare(strict_types = 1);
+
+use App\Invoice;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
-$dotenv->load();
 
-session_start();
+$invoice1 = new Invoice();
+$invoice2 = $invoice1;
 
-define('STORAGE_PATH', __DIR__ . '/../storage');
-define('VIEW_PATH', __DIR__ . '/../views');
+echo 'Unsetting  invoice 1' . PHP_EOL;
+unset($invoice1);
 
-$container = new Container();
-$router = new Router($container);
+var_dump($invoice2);
 
-$router
-    ->get('/', [App\Controllers\HomeController::class, 'index'])
-    ->post('/upload', [App\Controllers\HomeController::class, 'upload'])
-    ->get('/invoices', [App\Controllers\InvoiceController::class, 'index'])
-    ->get('/invoices/create', [App\Controllers\InvoiceController::class, 'create'])
-    ->post('/invoices/create', [App\Controllers\InvoiceController::class, 'store']);
-
-(new App\App(
-    $container,
-    $router,
-    ['uri' => $_SERVER['REQUEST_URI'],'method' => $_SERVER['REQUEST_METHOD']], 
-    
-    new Config($_ENV)
- ))->run();
-
-               
